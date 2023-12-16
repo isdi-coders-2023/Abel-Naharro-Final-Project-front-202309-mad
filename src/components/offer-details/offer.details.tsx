ed08ca4d6048d718e72fca021f444df5317ab8bb
abelnhm <abelnhm92@gmail.com>
2023-12-16 09:39:40 +0100

    Update component details

--- src/components/offer-details/offer.details.tsx
+++ src/components/offer-details/offer.details.tsx
@@ -1,77 +1,90 @@
 import { useParams } from 'react-router-dom';
-// import { Offer } from '../../model/offer';
 import './offer.details.scss';
-
-// type Props = {
-//   offerItem: Offer;
-// };
+import { useOffers } from '../../hooks/use.offers';
+import { Offer } from '../../model/offer';
+import { Loading } from '../loading/loading';
 
 export function OfferDetails() {
   const { id } = useParams();
-  console.log(id);
+  const { offers } = useOffers();
 
-  // export function OfferCard({ offerItem }: Props) {
-  // const date = new Date(offerItem.createdAt);
-  // const formattedDateOfCreated = date.toLocaleString('es-ES', {
-  //   day: '2-digit',
-  //   month: '2-digit',
-  //   year: 'numeric',
-  // });
+  if (offers.length === 0) {
+    // console.log(loadExternalOffer(id!));
+    // return <>Loading</>;
+    return <Loading />;
+  }
 
-  // const differencePrice = offerItem.offerPrice - offerItem.regularPrice;
-  // const percentageDiscount = (differencePrice / offerItem.regularPrice) * 100;
+  const offerItem = offers.find((item) => item.id === id) as Offer;
 
-  // const urlOffer = new URL(offerItem.offerURL);
-  // const hostNameOffer = urlOffer.host;
-  // const cleanHostNameOffer = hostNameOffer.replace('www.', '');
+  const date = new Date(offerItem.createdAt);
+  const formattedDateOfCreated = date.toLocaleString('es-ES', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
 
-  // let description = offerItem.description;
-  // if (description.length > 150) {
-  //   description = description.substring(0, 150) + '...';
-  // }
+  const differencePrice = offerItem.offerPrice - offerItem.regularPrice;
+  const percentageDiscount = (differencePrice / offerItem.regularPrice) * 100;
+
+  const urlOffer = new URL(offerItem.offerURL);
+  const hostNameOffer = urlOffer.host;
+  const cleanHostNameOffer = hostNameOffer.replace('www.', '');
+
+  let description = offerItem.description;
+  if (description.length > 150) {
+    description = description.substring(0, 150) + '...';
+  }
 
   return (
-    <section className="card-offer" role="contentinfo">
-      Details {id}
-      {/* <div className="content-image">
-        <img src={offerItem.image.cloudinaryURL} alt="offer image" />
-      </div>
-      <div className="content-info">
-        <div className="content-info-header">
-          <div className="buttons-vote">
-            <button className="btn-vote-down" title="vote-down">
-              <i className="fa-regular fa-thumbs-down"></i>
-            </button>
-            <span className="status">+1000</span>
-            <button className="btn-vote-up" title="vote-up">
-              <i className="fa-regular fa-thumbs-up"></i>
-            </button>
+    <>
+      {offerItem && (
+        <section className="card-offer-details" role="contentinfo">
+          <div>
+            <div className="content-image">
+              <img src={offerItem.image.cloudinaryURL} alt="offer image" />
+            </div>
+            <div className="content-info">
+              <div className="content-info-header">
+                <div className="buttons-vote">
+                  <button className="btn-vote-down" title="vote-down">
+                    <i className="fa-regular fa-thumbs-down"></i>
+                  </button>
+                  <span className="status">+1000</span>
+                  <button className="btn-vote-up" title="vote-up">
+                    <i className="fa-regular fa-thumbs-up"></i>
+                  </button>
+                </div>
+                <div className="created-at">
+                  <p>
+                    <i className="fa-regular fa-calendar-days"></i>{' '}
+                    {formattedDateOfCreated}
+                  </p>
+                </div>
+              </div>
+              <div className="title">{offerItem.title}</div>
+              <div className="subtitle">
+                <span className="offer-price">{offerItem.offerPrice}€</span>{' '}
+                <span className="regular-price">{offerItem.regularPrice}€</span>
+                <span className="discount">({percentageDiscount}%)</span>
+                <span className="brand-store">{cleanHostNameOffer}</span>
+              </div>
+              <div className="description">{description}</div>
+              <div className="author">
+                <img src="https://placehold.co/25x25/webp" alt="author" />
+                <p>{offerItem.author.userName}</p>
+              </div>
+              <div className="offer-link">
+                <a href={offerItem.offerURL} target="_blank" title="Link offer">
+                  Oppen Offer
+                </a>
+              </div>
+              {offerItem.isCoupon && (
+                <div className="offer-coupon">{offerItem.coupon}</div>
+              )}
+            </div>
           </div>
-          <div className="created-at">
-            <p>
-              <i className="fa-regular fa-calendar-days"></i>{' '}
-              {formattedDateOfCreated}
-            </p>
-          </div>
-        </div>
-        <div className="title">{offerItem.title}</div>
-        <div className="subtitle">
-          <span className="offer-price">{offerItem.offerPrice}€</span>{' '}
-          <span className="regular-price">{offerItem.regularPrice}€</span>
-          <span className="discount">({percentageDiscount}%)</span>
-          <span className="brand-store">{cleanHostNameOffer}</span>
-        </div>
-        <div className="description">{description}</div>
-        <div className="author">
-          <img src="https://placehold.co/25x25/webp" alt="author" />
-          <p>{offerItem.author.userName} - pendiente</p>
-        </div>
-        <div className="offer-link">
-          <a href="#">
-            Go to Offer <i className="fa-solid fa-up-right-from-square"></i>
-          </a>
-        </div>
-      </div> */}
-    </section>
+        </section>
+      )}
+    </>
   );
 }
