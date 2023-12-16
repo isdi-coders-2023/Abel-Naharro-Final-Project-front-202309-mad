b528c9832753dc8eb15fd0366c132ed6684b3965
abelnhm <abelnhm92@gmail.com>
2023-12-16 17:26:44 +0100

    Add crud routes, profile and control all routers with token

--- src/components/router/app.routes.tsx
+++ src/components/router/app.routes.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useUsers } from '../../hooks/use.users';
 
 const HomePage = lazy(() => import('../pages/home/home'));
 const LoginPage = lazy(() => import('../pages/login/login'));
@@ -7,19 +8,40 @@ const RegisterPage = lazy(() => import('../pages/register/register'));
 const CreateOfferPage = lazy(() => import('../pages/create/create'));
 const OfferPage = lazy(() => import('../pages/details/details'));
 const ErrorPage = lazy(() => import('../pages/error/error'));
+const EditOfferPage = lazy(() => import('../pages/edit/edit'));
+const UserProfile = lazy(() => import('../pages/profile/profile'));
+const ConfirmDelete = lazy(() => import('../pages/delete/delete'));
 
 export function AppRoutes() {
+  const { loggedUser } = useUsers();
+
   return (
     <Suspense>
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/sign-in" element={<LoginPage></LoginPage>}></Route>
         <Route path="/sign-up" element={<RegisterPage></RegisterPage>}></Route>
-        <Route
-          path="/share"
-          element={<CreateOfferPage></CreateOfferPage>}
-        ></Route>
         <Route path="/offer/:id" element={<OfferPage></OfferPage>}></Route>
+        {loggedUser && (
+          <>
+            <Route
+              path="/offer/share"
+              element={<CreateOfferPage></CreateOfferPage>}
+            ></Route>
+            <Route
+              path="/offer/edit/:id"
+              element={<EditOfferPage></EditOfferPage>}
+            ></Route>
+            <Route
+              path="/offer/delete/:id"
+              element={<ConfirmDelete></ConfirmDelete>}
+            ></Route>
+            <Route
+              path="/my-profile"
+              element={<UserProfile></UserProfile>}
+            ></Route>
+          </>
+        )}
         <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
       </Routes>
     </Suspense>
