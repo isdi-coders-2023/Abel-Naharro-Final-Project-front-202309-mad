50f232e8bca9c09c7211e76f2595bfa204b16bd1
abelnhm <abelnhm92@gmail.com>
2023-12-16 17:20:38 +0100

    Add component loading

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -3,7 +3,13 @@ import './loading.scss';
 export function Loading() {
   return (
     <section className="loading" role="contentinfo">
-      <div className="lds-ring">
+      {/* <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div> */}
+      <div className="lds-ellipsis">
         <div></div>
         <div></div>
         <div></div>
