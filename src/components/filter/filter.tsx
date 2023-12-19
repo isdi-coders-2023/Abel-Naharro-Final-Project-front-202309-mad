0b17886f90163c3ec020222c20a1de42ffc88a22
abelnhm <abelnhm92@gmail.com>
2023-12-19 10:58:18 +0100

    Add filter last offers

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,18 +1,40 @@
+import { SyntheticEvent } from 'react';
+import { useOffers } from '../../hooks/use.offers';
 import './filter.scss';
+import { Loading } from '../loading/loading';
 
 export function Filter() {
+  const { loadByCategory, offers } = useOffers();
+  const handleClickFilterByLastOffers = (
+    event: SyntheticEvent,
+    category: string
+  ) => {
+    event.preventDefault();
+
+    if (offers.length === 0) {
+      return <Loading />;
+    }
+
+    loadByCategory(category);
+  };
+
   return (
     <section className="filter" role="search">
       <div className="container">
         <h2>Filter</h2>
         <ul>
-          <li>
+          {/* <li>
             <a href="#" className="filter-on-fire">
               <i className="fa-solid fa-fire"></i> Offers On Fire!
             </a>
-          </li>
+          </li> */}
           <li>
-            <a href="#">Last Offers</a>
+            <a
+              href="#"
+              onClick={(event) => handleClickFilterByLastOffers(event, 'all')}
+            >
+              <i className="fa-solid fa-fire"></i> All Latest Offers
+            </a>
           </li>
         </ul>
       </div>
